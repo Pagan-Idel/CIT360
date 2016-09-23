03af025f374025c99672085a8a6f2fb2e37a1108
LastN <semiskill1@yahoo.com>
2016-09-23 12:39:28 -0600

    modified:   src/sandbox/threads1.java

--- src/sandbox/threads1.java
+++ src/sandbox/threads1.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
+//Sandbox code for threads, creation of thread class
+
 public class threads1 implements Runnable {
+	//Declare variables, name of thread and random time for thread to sleep.
 	String name;
 	int time;
 	Random r = new Random();
@@ -10,14 +13,31 @@ public class threads1 implements Runnable {
 		time = r.nextInt(999);
 	}
 	
+	//Runnable run method to create thread.
 	public void run(){
 		try{
 			System.out.printf("%s sleeping for %d\n", name, time);
 			Thread.sleep(time);
 			System.out.printf("%s is done\n", name);
+			//Return some specific thread properties
+			System.out.println(Thread.currentThread().getName());
+			System.out.println(Thread.currentThread().toString());
+			System.out.println(Thread.currentThread().isInterrupted());
 		}
 		catch(Exception e) {
-			
+			e.printStackTrace();
 		}
+	}
+	
+	//Main thread creation
+	public static void main(String[] args){
+		Thread t1 = new Thread(new threads1("first"));
+		Thread t2 = new Thread(new threads1("second"));
+		Thread t3 = new Thread(new threads1("third"));
+		
+	//Run Threads
+		t1.start();
+		t2.start();
+		t3.start();
 	}
 }
